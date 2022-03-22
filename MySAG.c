212b7c530e7f80892892c1959e47749c78d9d753
João Oliveira <joao.p.oliveira@ua.pt>
2022-03-22 10:23:04 +0000

    modified:   MySAG.c 	modified:   MySAG.h 	deleted:    Main.c 	deleted:    doc/html/Primeiro_8c.html 	deleted:    doc/html/Primeiro_8c__incl.md5 	deleted:    doc/html/Primeiro_8c__incl.png 	deleted:    doc/html/bc_s.png 	deleted:    doc/html/bdwn.png 	deleted:    doc/html/closed.png 	deleted:    doc/html/doc.png 	deleted:    doc/html/doxygen.css 	deleted:    doc/html/doxygen.svg 	deleted:    doc/html/dynsections.js 	deleted:    doc/html/files.html 	deleted:    doc/html/folderclosed.png 	deleted:    doc/html/folderopen.png 	deleted:    doc/html/globals.html 	deleted:    doc/html/globals_func.html 	deleted:    doc/html/graph_legend.html 	deleted:    doc/html/graph_legend.md5 	deleted:    doc/html/graph_legend.png 	deleted:    doc/html/index.html 	deleted:    doc/html/jquery.js 	deleted:    doc/html/menu.js 	deleted:    doc/html/menudata.js 	deleted:    doc/html/nav_f.png 	deleted:    doc/html/nav_g.png 	deleted:    doc/html/nav_h.png 	deleted:    doc/html/open.png 	deleted:    doc/html/search/all_0.html 	deleted:    doc/html/search/all_0.js 	deleted:    doc/html/search/all_1.html 	deleted:    doc/html/search/all_1.js 	deleted:    doc/html/search/all_2.html 	deleted:    doc/html/search/all_2.js 	deleted:    doc/html/search/close.svg 	deleted:    doc/html/search/files_0.html 	deleted:    doc/html/search/files_0.js 	deleted:    doc/html/search/functions_0.html 	deleted:    doc/html/search/functions_0.js 	deleted:    doc/html/search/functions_1.html 	deleted:    doc/html/search/functions_1.js 	deleted:    doc/html/search/mag_sel.svg 	deleted:    doc/html/search/nomatches.html 	deleted:    doc/html/search/search.css 	deleted:    doc/html/search/search.js 	deleted:    doc/html/search/search_l.png 	deleted:    doc/html/search/search_m.png 	deleted:    doc/html/search/search_r.png 	deleted:    doc/html/search/searchdata.js 	deleted:    doc/html/splitbar.png 	deleted:    doc/html/sync_off.png 	deleted:    doc/html/sync_on.png 	deleted:    doc/html/tab_a.png 	deleted:    doc/html/tab_b.png 	deleted:    doc/html/tab_h.png 	deleted:    doc/html/tab_s.png 	deleted:    doc/html/tabs.css 	deleted:    doc/latex/Makefile 	deleted:    doc/latex/Primeiro_8c.tex 	deleted:    doc/latex/Primeiro_8c__incl.md5 	deleted:    doc/latex/Primeiro_8c__incl.pdf 	deleted:    doc/latex/doxygen.sty 	deleted:    doc/latex/files.tex 	deleted:    doc/latex/longtable_doxygen.sty 	deleted:    doc/latex/refman.tex 	deleted:    doc/latex/tabu_doxygen.sty 	modified:   makefile
    	app

--- MySAG.c
+++ MySAG.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
-#include "mySAG.h"
+#include "MySAG.h"
 
-global  int stream[] = {}
+static stream[100];
 
 void MySAGInsert()
 {
-
+	static int pos=0;
+	printf("Valor a inserir: ");
+	scanf("%d",&stream[pos]);
+	pos++;
 }
 
-void MysagInit()
+void MySAGInit()
 {
+	int opc;
+	while(1)
+	{
+		printf("Escolha uma opção:\n");
+		printf("1:Adicionar Valor\n");
+		printf("2:Valor Maximo\n");
+		printf("3:Valor Minimo\n");
+		printf("4:Valor Medio\n");
+		printf("5:Frequencia Valor\n");
+		printf("0:Sair\n");
+		scanf("%d",&opc);
+		if(opc==0)
+			break;
+		else if(opc==1)
+			MySAGInsert();
+		//else if(opc==2)
+			//MySAGMax();
+		//else if(opc==3)
+			//MySAGMin();
+		//else if(opc==4)
+			//MySAGAvg();
+		//else if(opc==5)
+			//MySAGFreq();
+		else
+			printf("Opção Invalida\n");
 
+	}
+	printf("Saiu\n");
+}
+
+int main(void)
+{
+	MySAGInit();
 }
---
+++ MySAG.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+void MySAGInit();
+void MySAGInsert();
